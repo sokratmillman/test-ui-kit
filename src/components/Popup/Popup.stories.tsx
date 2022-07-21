f2579d9cebc8852784b5d0cf3ef77785cf346943
sokratmillman <superstepashka1981@gmail.com>
2022-07-21 17:37:29 +0300

    docs: add story for Popup component

---
+++ src/components/Popup/Popup.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Popup from './Popup';
+
+export default {
+  title: 'Popup',
+};
+
+export function Default(props: any) {
+  return <Popup {...props}>Button</Popup>;
+}
+
+Default.argTypes = {
+  onPress: { action: 'press' },
+  onPressStart: { action: 'press start' },
+  onPressEnd: { action: 'press end' },
+  onPressUp: { action: 'press up' },
+};
+
+Default.args = {
+  disabled: false,
+};
